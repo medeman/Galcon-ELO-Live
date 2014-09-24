4c408287b4f398749f57609105b828c319e0f83d
Medeman <medemanx@googlemail.com>
2014-09-24 19:05:38 +0200

    Added addGame function to Player class

--- classes/Player.php
+++ classes/Player.php
@@ -9,6 +9,14 @@ class Player {
 	private $gamesPlayed = 0;
 	private $gamesWon = 0;
 	private $gamesLost = 0;
+	
+	public function addGame($winner) {
+		if ($winner) {
+			++$gamesWon;
+		} else ++$gamesLost;
+		
+		++$gamesPlayed;
+	}
 }
 
 ?>
